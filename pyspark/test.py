23a195c1881b6bdea46ef77c49ba4b242937bc0f
Vegard <vegardmatre98@gmail.com>
2022-03-04 13:04:47 +0100

    Add a text file to test

--- pyspark/test.py
+++ pyspark/test.py
@@ -1,7 +1,8 @@
 import pyspark
+
 sc = pyspark.SparkContext('local[*]')
 
-txt = sc.textFile('file:////usr/share/doc/python/copyright')
+txt = sc.textFile('lorem_ipsum.txt')
 python_lines = txt.filter(lambda line: 'python' in line.lower())
 
 with open('results.txt', 'w') as file_obj:
