8a4a58423dac1bc81ca3c36f7b4e36372f513c43
Vegard <vegardmatre98@gmail.com>
2022-03-04 12:53:10 +0100

    Add a pyspark folder with a test.py file

---
+++ pyspark/test.py
@@ -0,0 +1,10 @@
+import pyspark
+sc = pyspark.SparkContext('local[*]')
+
+txt = sc.textFile('file:////usr/share/doc/python/copyright')
+python_lines = txt.filter(lambda line: 'python' in line.lower())
+
+with open('results.txt', 'w') as file_obj:
+    file_obj.write(f'Number of lines: {txt.count()}\n')
+    file_obj.write(f'Number of lines with python: {python_lines.count()}\n')
+
